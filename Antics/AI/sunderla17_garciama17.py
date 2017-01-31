98c0474ad0956135ae4687102f8fa08779110751
Elise Sunderland <sunderla17@up.edu>
2017-01-30 17:13:15 -0800

    Clean up, comments added
    comments added and soldier's path finding cleared up. They chase the
    queen when they can

--- Antics/AI/sunderla17_garciama17.py
+++ Antics/AI/sunderla17_garciama17.py
@@ -14,7 +14,7 @@ from AIPlayerUtils import *
 #AIPlayer
 #Description: The responsbility of this class is to interact with the game by
 #deciding a valid move based on a given game state. This class has methods that
-#will be implemented by students in Dr. Nuxoll's AI course.
+#have been implemented by Elise Sunderland and Matt Garcia
 #
 #Variables:
 #   playerId - The id of the player.
@@ -29,8 +29,8 @@ class AIPlayer(Player):
     ##
     def __init__(self, inputPlayerId):
         super(AIPlayer,self).__init__(inputPlayerId, "Heuristic Worker")
-        #the coordinates of the agent's food and tunnel will be stored in these
-        #variables (see getMove() below)
+
+        #Useful instance variables
         self.myFood = None
         self.myFood1 = None
         self.myFood2 = None
@@ -38,17 +38,17 @@ class AIPlayer(Player):
         self.myAnthill = None
         self.enemyAnthill = None
         self.enemyTunnel = None
-        #self.playerId = imputPlayerId
 
     ##
     #getPlacement
     #
-    # The agent uses a hardcoded arrangement for phase 1 to provide maximum
-    # protection to the queen.  Enemy food is placed randomly.
-    #
+    # This agent tries to place the food in the least optimal places
+    # The Constructions are placed to try and be as balanced as it can.
     #
     #Adjust this to set some more optimal placement
     def getPlacement(self, currentState):
+
+        #keep track of these
         self.myFood = None
         self.myFood1 = None
         self.myFood2 = None
@@ -79,34 +79,33 @@ class AIPlayer(Player):
                 for j in range(0,9):
                     for k in range (6,9):
                         move = None
-                        #while move == None:
-                            #Find the farthest open space
+                        #Find the farthest open space
                         if(((stepsToReach(currentState, (j,k), self.enemyTunnel.coords)) + (stepsToReach(currentState, (j,k), self.enemyAnthill.coords)))\
-                                > ((stepsToReach(currentState, moves, self.enemyTunnel.coords)) + (stepsToReach(currentState, moves, self.enemyAnthill.coords)))):
-                            #moves = (j,k)
-                                #Set the move if this space is empty
-                                if currentState.board[j][k].constr == None and (j, k) not in moves:
-                                    move = (j, k)
-                                    #Just need to make the space non-empty. So I threw whatever I felt like in there.
-                                    currentState.board[j][k].constr == True
-                                    moves.append(move)
+                            > ((stepsToReach(currentState, moves, self.enemyTunnel.coords)) + (stepsToReach(currentState, moves, self.enemyAnthill.coords)))):
+                            #Set the move if this space is empty
+                            if currentState.board[j][k].constr == None and (j, k) not in moves:
+                                move = (j, k)
+                                #Just need to make the space non-empty. So I threw whatever I felt like in there.
+                                currentState.board[j][k].constr == True
+                                moves.append(move)
             return moves
 
 
     ##
     #getMove
     #
-    #
-    #Adjust this to create more ants and start with a greater gathering force to get food faster
-    #also try to adjust efficieny and opt for the best paths and ants for the gathering job
+    # This agent builds 3 workers, and soldiers as necessary if the opponent is mean
     ##
     def getMove(self, currentState):
+
         #Useful pointers
         myInv = getCurrPlayerInventory(currentState)
         me = currentState.whoseTurn
         foods = getConstrList(currentState, None, (FOOD,))
         bestDistSoFar = 1000 #i.e., infinity
         bestDistSoFar2 = 1000 #i.e., infinity
+
+        #keep track of our ants
         numWorkers = len(getAntList(currentState, me, (WORKER,)))
         numSoldiers = len(getAntList(currentState, me, (SOLDIER,)))
 
@@ -133,25 +132,25 @@ class AIPlayer(Player):
                     self.myFood2 = food
                     bestDistSoFar2 = dist2
 
-        #if the queen is on the anthill move her
+        #if the queen is on the anthill move her away to the back row
         if (myInv.getQueen().coords == myInv.getAnthill().coords):
             path = createPathToward(currentState, myInv.getQueen().coords,
                                         (0,3), UNIT_STATS[QUEEN][MOVEMENT])
             return Move(MOVE_ANT, path, None)
 
-        #if I have enough food, build another worker to gather food.
-        #Don't build more than 2 workers.
+        #if I have enough food, build more workers
+        #Don't build more than 3 workers.
         if (myInv.foodCount > 1 and numWorkers < 3):
             if (getAntAt(currentState, myInv.getAnthill().coords) is None):
                 return Move(BUILD, [myInv.getAnthill().coords], WORKER)
 
-        #Then Soldiers whenever we are able, if our opponent has any attacking units
-        if (myInv.foodCount > 3 and numSoldiers < 2):
-            if (getAntAt(currentState, myInv.getAnthill().coords) is None):
-                return Move(BUILD, [myInv.getAnthill().coords], SOLDIER)
+        #Then Soldiers whenever we are able, if our opponent has any attacking units. Don't build more than 2 soldiers.
+        if not (getAntList(currentState, me+1, (DRONE, SOLDIER)) is None):
+            if (myInv.foodCount > 3 and numSoldiers < 2):
+                if (getAntAt(currentState, myInv.getAnthill().coords) is None):
+                    return Move(BUILD, [myInv.getAnthill().coords], SOLDIER)
 
-
-       #WORKER Orders
+       #WORKER Orders. They gather food as fast as they can.
         myWorkers = getAntList(currentState, me, (WORKER,))
         for index, worker in enumerate(myWorkers):
             if (worker.hasMoved): continue
@@ -179,7 +178,7 @@ class AIPlayer(Player):
                     path = listAllMovementPaths(currentState,worker.coords, UNIT_STATS[WORKER][MOVEMENT])[0]
                 return Move(MOVE_ANT, path, None)
 
-        #move our soldiers. They all try to find the queen
+        #SOLDIER orders. They move to the enemy side and bother the queen
         mySoldiers = getAntList(currentState, me, (SOLDIER,))
         for soldier in mySoldiers:
             if not (soldier.hasMoved):
@@ -188,24 +187,24 @@ class AIPlayer(Player):
                 if (mySoldierY < 8): #move to enemy's side
                     mySoldierY += 1
                 else:
-                    mySoldierX += 1
                     #find the queen
                     enemyQueen = getAntList(currentState, None, (QUEEN,))
                     #create a path toward the queen
-                    #print enemyQueen[0].coords
-                    soldierPath = createPathToward(currentState, soldier.coords,
+                    soldierPath = createPathToward(currentState, (mySoldierX, mySoldierY),
                                                  enemyQueen[0].coords, UNIT_STATS[SOLDIER][MOVEMENT])
-                    Move(MOVE_ANT, soldierPath, None)
+                    return Move(MOVE_ANT, soldierPath, None)
                 if (mySoldierX,mySoldierY) in listReachableAdjacent(currentState, soldier.coords, 2):
                     return Move(MOVE_ANT, [soldier.coords, (mySoldierX, mySoldierY)], None)
                 else:
                     return Move(MOVE_ANT, [soldier.coords], None)
-        #If our move hasnt been ended, end our move here.
+
+        #If our move hasnt been ended by now, end our move here.
         return Move(END, None, None)
     ##
     #getAttack
     #
-    # This agent never attacks
+    # This agent has no rhyme or reason to how it attacks. takes the first
+    # enemy it sees
     #
     def getAttack(self, currentState, attackingAnt, enemyLocations):
         return enemyLocations[0]  #don't care
