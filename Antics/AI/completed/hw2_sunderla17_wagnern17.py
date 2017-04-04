cd9ad45f86f5108e647d039dc3747f88b8d1c257
Elise S <sunderla17@up.edu>
2017-04-03 17:40:48 -0700

    started HW5
    fun shot!!

--- Antics/AI/completed/hw2_sunderla17_wagnern17.py
+++ Antics/AI/completed/hw2_sunderla17_wagnern17.py
@@ -29,7 +29,7 @@ class AIPlayer(Player):
     #   inputPlayerId - The id to give the new player (int)
     ##
     def __init__(self, inputPlayerId):
-        super(AIPlayer,self).__init__(inputPlayerId, "MiniMax Search")
+        super(AIPlayer,self).__init__(inputPlayerId, "A* Search")
         #the coordinates of the agent's food and tunnel will be stored in these
         #variables (see getMove() below)
         self.myFood = None
@@ -84,12 +84,13 @@ class AIPlayer(Player):
     ##
     #getMove
     #
-    # This gets the move from the list of recursive moves generated
+    # This gets the move from the list of recursive moves generated for A* Search
     # returns the selected move from that traversal
     ##
     def getMove(self, currentState):
-        #recursively find moves, setting max to true as initial
-        selectedMove = self.recursiveMove(currentState, 0, True)
+        self.evaluateState(currentState)
+
+        selectedMove = self.recursiveMove(currentState, 0)
         if (selectedMove == None):
             return Move(END, None, None)
 
@@ -110,8 +111,13 @@ class AIPlayer(Player):
     # This function behaves as the heuristic evaluation for various states the agent can enter
     # returns a value from 0.0 - 1.0 to indicate how much of a "winning" move has been selected
     #
-    # 0.0 means the enemy has won the game. > 0.5 means you are currently winning
+    # 0.0 means the enemy has won the game.
+    # > 0.5 means you are currently winning
     #
+    # Currently, this function operates solely on the basis of gathering food for simplicity
+    # and time conservation.
+    #
+    # gets passed the current game state object.
     #
     def evaluateState(self, currentState):
         #useful pointers. Set state score to a default value since we interpret 0 as losing.
@@ -123,16 +129,22 @@ class AIPlayer(Player):
         randFloat = random.uniform(0.0, 0.0001)
 
         #Metrics that are important to evaluate:
+        # number of ants each player has
+        myAnts = getAntList(currentState, me,(QUEEN, WORKER, DRONE, SOLDIER, R_SOLDIER))
+        myAntCount = len(getAntList(currentState, me,(QUEEN, WORKER, DRONE, SOLDIER, R_SOLDIER)))
+
         # types of ants each player has. Right now we simply care about workers
         myWorkers = getAntList(currentState, me,(WORKER,))
+
+        #more than 2 workers is good
+        if (len(myWorkers) >= 2 and len(myWorkers) < 4):
+            stateScore = 0.9
 
         # How much food each player has
         publicFood = getConstrList(currentState, None, (FOOD,))
         myFood = myInv.foodCount
         enemyFood = enemyInv.foodCount
         foodDiff = myFood - enemyFood
-
-        foodScore = 0.0
 
         if (myFood != 0):
             foodScore = myFood/10.0
@@ -171,110 +183,84 @@ class AIPlayer(Player):
 
         avgWorkerScore = workerScore/len(myWorkers)
 
-        # Calculate a score for the queen. Take into account the threat level
+        # move my queen off of the anthill
         myQueen = getAntList(currentState, me, (QUEEN,))[0]
         queenScore = 0
-
-        #get my queen off of the anthill
         stepsToAnthill = stepsToReach(currentState, myQueen.coords, myInv.getAnthill().coords)
         stepsToTunnel = stepsToReach(currentState, myQueen.coords, myInv.getTunnels()[0].coords)
         if (stepsToAnthill < 2 or stepsToTunnel < 2):
-            if (queenScore > 0.2):
+            if (queenScore > 0.1):
                 queenScore -= 0.2
         else:
-            # add threat level code here
             queenScore += 0.3
 
         #return the state score as a weighted average of the food and worker scores
-        # based on whose turn it is, return this as a positive or negative score.
         stateScore = (foodScore * 0.95)+(avgWorkerScore*0.08)+(queenScore* 0.2)
         if (stateScore < (1 - 0.0001)):
             stateScore += randFloat
         return stateScore
+    ##
+    # bestNodeScore
+    #
+    # takes a list of state nodes and returns the best score
+    # the best score should help the choice of next state.
+    #
+    def bestNodeScore(self, nodeList):
+        #make a list of just the scores from the nodes
+        scoreList = [x["score"] for x in nodeList]
+
+        #make sure list is non-empty
+        for score in scoreList:
+            if (max(scoreList) == score):
+                return score
+            else:
+                return max(scoreList)
 
     ##
     # recursiveMove <!-- RECURSIVE -->
     #
-    # This function generates our MiniMax tree nodes
+    # This function generates our A* node tree to traverse.
     #
     # We recurse on the currentState object with the depth + 1
     # State nodes are represented by a python Dictionary
     #
-    # this method also performs a form of alpha/beta pruning.
-    #
-    def recursiveMove(self, currentState, currentDepth, max):
-        me = currentState.whoseTurn
-        enemy = (currentState.whoseTurn + 1) % 2
-
+    def recursiveMove(self, currentState, currentDepth):
         #generate list of all available moves. Leave out END TURN moves as a part of this
         moveList = listAllMovementMoves(currentState)
+        if (len(moveList) < 1):
+            return None
+
         #generate the list of all available nodes with a dictionary
         nodeList = [dict({'move': None, 'nextState': None, 'score':None}) for x in range(len(moveList))]
         for i, move in enumerate(moveList):
             #populate the generated empty dicts with the right stats
             nodeList[i]['move'] = move
-            nodeList[i]['nextState'] = getNextStateAdversarial(currentState, move)
+            nodeList[i]['nextState'] = getNextState(currentState, move)
             nodeList[i]['score'] = self.evaluateState(nodeList[i]['nextState'])
 
         if (len(nodeList) == 0):
             #if we can make no more moves, return an END_TURN move type
-            endMove = Move(END, None, None)
-            nextState = getNextStateAdversarial(currentState, endMove)
-            #switch to the min player flag
-            max = not max
-            #recurse with this new player flag
-            return self.recursiveMove(nextState, currentDepth, max)
+            nodeList = [dict({'move': Move(END, None, None), 'nextState': None, 'score': None})]
 
-        #base case: return the state score, regardless of player
-        if (currentDepth >= self.maxDepth):
-            #return the score
+        #base case: return the state score
+        if (currentDepth > self.maxDepth):
             return self.evaluateState(currentState)
 
-        # if the depth is 0, we return the move with the best score that got us there
+        #if we are at the head of the tree, return best scored node's move
         elif (currentDepth == 0):
-            bestMove = Move(END, None, None)
-            bestScore = 0.0
-            beta = 0.0
+            currScore = self.bestNodeScore(nodeList) #highest score
             for node in nodeList:
-                #store the score as "best" minimax score
-                currScore = self.recursiveMove(node['nextState'], currentDepth + 1, max) #returns a score for each node
-                #compare current score to the best one that ive seen
-                if (max):
-                    if (currScore > bestScore):
-                        bestScore = currScore
-                        #take that move that got you there
-                        bestMove = node['move']
-                    elif (beta <= currScore): #we are out of the cutoff range
-                        break
-                else:
-                    #set the beta value to the best possible thing
-                    beta = 1.0
-                    bestScore = min(currScore, beta)
-                    break
-            #return the best move
-            return bestMove
-
-        #we are somewhere in the middle of the tree, get the score
+                #find the node with this score
+                if (node['score'] == currScore):
+                    return node['move']
+        #we are somewhere in the middle of the tree, recurse
         else:
-            bestScore = 0.0
-            beta = 0.0
+            #recursively call this on the state with the highest score
+            currScore = self.bestNodeScore(nodeList) #highest score
             for node in nodeList:
-                #store the score as "best" minimax score
-                currScore = self.recursiveMove(node['nextState'], currentDepth + 1, max) #returns a score for each node
-                #compare current score to the best one that ive seen
-                if (max):
-                    if (currScore > bestScore):
-                        bestScore = currScore
-                    elif (beta <= currScore):
-                        #break out, we should prune since we've passed cutoff
-                        break
-                else:
-                    #prune anything outside of this range
-                    beta = 1.0
-                    bestScore = min(currScore, beta)
-                    break
-            #return the best score we can find
-            return bestScore
+                #find the node with this score
+                if (node['score'] == currScore):
+                    return self.recursiveMove(node['nextState'], currentDepth + 1)
     ##
     #registerWin
     #
--- Antics/AI/hw5_start.py
+++ Antics/AI/hw5_start.py
@@ -1,6 +1,7 @@
   # -*- coding: latin-1 -*-
 import random
 import sys
+import numpy as np
 sys.path.append("..")  #so other modules can be found in parent dir
 from Player import *
 from Constants import *
@@ -29,12 +30,11 @@ class AIPlayer(Player):
     #   inputPlayerId - The id to give the new player (int)
     ##
     def __init__(self, inputPlayerId):
-        super(AIPlayer,self).__init__(inputPlayerId, "MiniMax Search")
+        super(AIPlayer,self).__init__(inputPlayerId, "Neural Networks")
         #the coordinates of the agent's food and tunnel will be stored in these
-        #variables (see getMove() below)
-        self.myFood = None
-        self.myTunnel = None
-        self.maxDepth = 2
+        self.targets = []
+        self.inputs = []
+        self.maxDepth = 2;
 
     ##
     #getPlacement
@@ -84,17 +84,45 @@ class AIPlayer(Player):
     ##
     #getMove
     #
-    # This gets the move from the list of recursive moves generated
+    # This gets the move from the list of recursive moves generated for A* Search
     # returns the selected move from that traversal
     ##
     def getMove(self, currentState):
-        #recursively find moves, setting max to true as initial
-        selectedMove = self.recursiveMove(currentState, 0, True)
+        self.evaluateState(currentState)
+
+        selectedMove = self.recursiveMove(currentState, 0)
         if (selectedMove == None):
             return Move(END, None, None)
 
         return selectedMove
 
+    # sigmoid function for backpropigation, generated from numpy
+    def nonlin(self, x, deriv=False):
+        if(deriv==True):
+            return x*(1-x)
+        return 1/(1+np.exp(-x))
+
+    def backPropigate(self):
+        #seed random numbers for deterministic training?
+        np.random.seed(1)
+
+        #initialize random weights on all of our inputs, average is 0
+        syn0 = 2*np.random.random((3,1)) - 1
+
+        for iter in xrange(10):
+
+            #forward propogation
+            l0 = self.inputs ## this is currently empty, generated from our mapping function
+            l1 = self.nonlin(np.dot(l0, syn0)) #should generate output?
+
+            #error
+            l1_error = targets - l1 #remember 1 output per 14 inputs
+
+            #delta values
+            l1_delta = l1_error * nonlin(l1,True)
+
+            #update weights ----> to be hardcoded later?
+            syn0 += np.dot(l0.T,l1_delta)
 
     ##
     #getAttack
@@ -110,8 +138,13 @@ class AIPlayer(Player):
     # This function behaves as the heuristic evaluation for various states the agent can enter
     # returns a value from 0.0 - 1.0 to indicate how much of a "winning" move has been selected
     #
-    # 0.0 means the enemy has won the game. > 0.5 means you are currently winning
+    # 0.0 means the enemy has won the game.
+    # > 0.5 means you are currently winning
     #
+    # Currently, this function operates solely on the basis of gathering food for simplicity
+    # and time conservation.
+    #
+    # gets passed the current game state object.
     #
     def evaluateState(self, currentState):
         #useful pointers. Set state score to a default value since we interpret 0 as losing.
@@ -123,8 +156,16 @@ class AIPlayer(Player):
         randFloat = random.uniform(0.0, 0.0001)
 
         #Metrics that are important to evaluate:
+        # number of ants each player has
+        myAnts = getAntList(currentState, me,(QUEEN, WORKER, DRONE, SOLDIER, R_SOLDIER))
+        myAntCount = len(getAntList(currentState, me,(QUEEN, WORKER, DRONE, SOLDIER, R_SOLDIER)))
+
         # types of ants each player has. Right now we simply care about workers
         myWorkers = getAntList(currentState, me,(WORKER,))
+
+        #more than 2 workers is good
+        if (len(myWorkers) >= 2 and len(myWorkers) < 4):
+            stateScore = 0.9
 
         # How much food each player has
         publicFood = getConstrList(currentState, None, (FOOD,))
@@ -132,14 +173,13 @@ class AIPlayer(Player):
         enemyFood = enemyInv.foodCount
         foodDiff = myFood - enemyFood
 
-        foodScore = 0.0
-
         if (myFood != 0):
             foodScore = myFood/10.0
 
         #definite lose state
         if(enemyFood == 10):
             stateScore = 0.0
+            self.targets.append(stateScore)
             return stateScore
 
         # give points if the workers are close to the tunnel/anthill with food
@@ -147,6 +187,7 @@ class AIPlayer(Player):
         workerScore = 0
         if (len(myWorkers) == 0):
             stateScore = 0.1 #all of my workers are dead
+            self.targets.append(stateScore)
             return stateScore
 
         for worker in myWorkers:
@@ -171,110 +212,87 @@ class AIPlayer(Player):
 
         avgWorkerScore = workerScore/len(myWorkers)
 
-        # Calculate a score for the queen. Take into account the threat level
+        # move my queen off of the anthill
         myQueen = getAntList(currentState, me, (QUEEN,))[0]
         queenScore = 0
-
-        #get my queen off of the anthill
         stepsToAnthill = stepsToReach(currentState, myQueen.coords, myInv.getAnthill().coords)
         stepsToTunnel = stepsToReach(currentState, myQueen.coords, myInv.getTunnels()[0].coords)
         if (stepsToAnthill < 2 or stepsToTunnel < 2):
-            if (queenScore > 0.2):
+            if (queenScore > 0.1):
                 queenScore -= 0.2
         else:
-            # add threat level code here
             queenScore += 0.3
 
         #return the state score as a weighted average of the food and worker scores
-        # based on whose turn it is, return this as a positive or negative score.
         stateScore = (foodScore * 0.95)+(avgWorkerScore*0.08)+(queenScore* 0.2)
         if (stateScore < (1 - 0.0001)):
             stateScore += randFloat
+
+        ##add this score to a list of "test scores" for our neural network to learn from
+        self.targets.append(stateScore)
         return stateScore
+    ##
+    # bestNodeScore
+    #
+    # takes a list of state nodes and returns the best score
+    # the best score should help the choice of next state.
+    #
+    def bestNodeScore(self, nodeList):
+        #make a list of just the scores from the nodes
+        scoreList = [x["score"] for x in nodeList]
+
+        #make sure list is non-empty
+        for score in scoreList:
+            if (max(scoreList) == score):
+                return score
+            else:
+                return max(scoreList)
 
     ##
     # recursiveMove <!-- RECURSIVE -->
     #
-    # This function generates our MiniMax tree nodes
+    # This function generates our A* node tree to traverse.
     #
     # We recurse on the currentState object with the depth + 1
     # State nodes are represented by a python Dictionary
     #
-    # this method also performs a form of alpha/beta pruning.
-    #
-    def recursiveMove(self, currentState, currentDepth, max):
-        me = currentState.whoseTurn
-        enemy = (currentState.whoseTurn + 1) % 2
-
+    def recursiveMove(self, currentState, currentDepth):
         #generate list of all available moves. Leave out END TURN moves as a part of this
         moveList = listAllMovementMoves(currentState)
+
         #generate the list of all available nodes with a dictionary
         nodeList = [dict({'move': None, 'nextState': None, 'score':None}) for x in range(len(moveList))]
         for i, move in enumerate(moveList):
             #populate the generated empty dicts with the right stats
             nodeList[i]['move'] = move
-            nodeList[i]['nextState'] = getNextStateAdversarial(currentState, move)
+            nodeList[i]['nextState'] = getNextState(currentState, move)
             nodeList[i]['score'] = self.evaluateState(nodeList[i]['nextState'])
 
         if (len(nodeList) == 0):
             #if we can make no more moves, return an END_TURN move type
-            endMove = Move(END, None, None)
-            nextState = getNextStateAdversarial(currentState, endMove)
-            #switch to the min player flag
-            max = not max
-            #recurse with this new player flag
-            return self.recursiveMove(nextState, currentDepth, max)
+            nodeList = [dict({'move': Move(END, None, None), 'nextState': None, 'score': None})]
+            #could call here with max == false, since that's the only time it gets set
+            return nodeList[0]['move']
 
-        #base case: return the state score, regardless of player
+        #base case: return the state score
         if (currentDepth >= self.maxDepth):
-            #return the score
             return self.evaluateState(currentState)
 
-        # if the depth is 0, we return the move with the best score that got us there
+        #if we are at the head of the tree, return best scored node's move
         elif (currentDepth == 0):
-            bestMove = Move(END, None, None)
-            bestScore = 0.0
-            beta = 0.0
+            currScore = self.bestNodeScore(nodeList) #highest score
             for node in nodeList:
-                #store the score as "best" minimax score
-                currScore = self.recursiveMove(node['nextState'], currentDepth + 1, max) #returns a score for each node
-                #compare current score to the best one that ive seen
-                if (max):
-                    if (currScore > bestScore):
-                        bestScore = currScore
-                        #take that move that got you there
-                        bestMove = node['move']
-                    elif (beta <= currScore): #we are out of the cutoff range
-                        break
-                else:
-                    #set the beta value to the best possible thing
-                    beta = 1.0
-                    bestScore = min(currScore, beta)
-                    break
-            #return the best move
-            return bestMove
-
-        #we are somewhere in the middle of the tree, get the score
+                #find the node with this score
+                if (node['score'] == currScore):
+                    return node['move']
+        #we are somewhere in the middle of the tree, recurse
         else:
-            bestScore = 0.0
-            beta = 0.0
+            #recursively call this on the state with the highest score
+            currScore = self.bestNodeScore(nodeList) #highest score
             for node in nodeList:
-                #store the score as "best" minimax score
-                currScore = self.recursiveMove(node['nextState'], currentDepth + 1, max) #returns a score for each node
-                #compare current score to the best one that ive seen
-                if (max):
-                    if (currScore > bestScore):
-                        bestScore = currScore
-                    elif (beta <= currScore):
-                        #break out, we should prune since we've passed cutoff
-                        break
-                else:
-                    #prune anything outside of this range
-                    beta = 1.0
-                    bestScore = min(currScore, beta)
-                    break
-            #return the best score we can find
-            return bestScore
+                #find the node with this score
+                if (node['score'] == currScore):
+                    return self.recursiveMove(node['nextState'], currentDepth + 1)
     ##
     #registerWin
     #
