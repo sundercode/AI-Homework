db2d58a7c082331cc2d81105aa42751ca59fbe25
Elise S <sunderla17@up.edu>
2017-04-18 09:52:44 -0700

    added hw6 files

---
+++ Antics/AI/hw6_start.py
@@ -0,0 +1,157 @@
+import random
+import sys
+sys.path.append("..")  #so other modules can be found in parent dir
+from Player import *
+from Constants import *
+from Construction import CONSTR_STATS
+from Ant import UNIT_STATS
+from Move import Move
+from GameState import *
+from AIPlayerUtils import *
+
+##
+#AIPlayer
+#Description: The responsbility of this class is to interact with the game by
+#deciding a valid move based on a given game state. This class has methods that
+#will be implemented by students in Dr. Nuxoll's AI course.
+#
+#Variables:
+#   playerId - The id of the player.
+##
+class AIPlayer(Player):
+
+    #__init__
+    #Description: Creates a new Player
+    #
+    #Parameters:
+    #   inputPlayerId - The id to give the new player (int)
+    ##
+    def __init__(self, inputPlayerId):
+        super(AIPlayer,self).__init__(inputPlayerId, "TD Learning Agent")
+        self.foodStates = 11*[None]
+
+    ##
+    #getPlacement
+    #
+    #Parameters:
+    #   construction - the Construction to be placed.
+    #   currentState - the state of the game at this point in time.
+    #
+    #Return: The coordinates of where the construction is to be placed
+    ##
+    def getPlacement(self, currentState):
+        numToPlace = 0
+        #implemented by students to return their next move
+        if currentState.phase == SETUP_PHASE_1:    #stuff on my side
+            numToPlace = 11
+            moves = []
+            for i in range(0, numToPlace):
+                move = None
+                while move == None:
+                    #Choose any x location
+                    x = random.randint(0, 9)
+                    #Choose any y location on your side of the board
+                    y = random.randint(0, 3)
+                    #Set the move if this space is empty
+                    if currentState.board[x][y].constr == None and (x, y) not in moves:
+                        move = (x, y)
+                        #Just need to make the space non-empty. So I threw whatever I felt like in there.
+                        currentState.board[x][y].constr == True
+                moves.append(move)
+            return moves
+        elif currentState.phase == SETUP_PHASE_2:   #stuff on foe's side
+            numToPlace = 2
+            moves = []
+            for i in range(0, numToPlace):
+                move = None
+                while move == None:
+                    #Choose any x location
+                    x = random.randint(0, 9)
+                    #Choose any y location on enemy side of the board
+                    y = random.randint(6, 9)
+                    #Set the move if this space is empty
+                    if currentState.board[x][y].constr == None and (x, y) not in moves:
+                        move = (x, y)
+                        #Just need to make the space non-empty. So I threw whatever I felt like in there.
+                        currentState.board[x][y].constr == True
+                moves.append(move)
+            return moves
+        else:
+            return [(0, 0)]
+
+    ##
+    #getMove
+    #Description: Gets the next move from the Player.
+    #Parameters:
+    #   currentState - The state of the current game waiting for the player's move (GameState)
+    #Return: The Move to be made
+    ##
+    def getMove(self, currentState):
+        moves = listAllLegalMoves(currentState)
+        selectedMove = moves[random.randint(0,len(moves) - 1)];
+
+        #don't do a build move if there are already 3+ ants
+        numAnts = len(currentState.inventories[currentState.whoseTurn].ants)
+        while (selectedMove.moveType == BUILD and numAnts >= 3):
+            selectedMove = moves[random.randint(0,len(moves) - 1)];
+
+        return selectedMove
+
+    ##
+    #getAttack
+    #Description: Gets the attack to be made from the Player
+    #
+    #Parameters:
+    #   currentState - A clone of the current state (GameState)
+    #   attackingAnt - The ant currently making the attack (Ant)
+    #   enemyLocation - The Locations of the Enemies that can be attacked (Location[])
+    ##
+    def getAttack(self, currentState, attackingAnt, enemyLocations):
+        #Attack a random enemy.
+        return enemyLocations[random.randint(0, len(enemyLocations) - 1)]
+
+    ##
+    #consolidateStates
+    #
+    # Description: this function will take a current state of the game and assign it to a predesignated state
+    # This is so we do not end up with tens of thousands of states by the end of an antics game
+    #
+    # Parameters: the current state of the game.
+    #
+    # Return: the corresponding "state group" that the current state belongs to.
+    #
+    def consolidateStates(self, currentState):
+        me = currentState.whoseTurn
+        myInv = currentState.inventories[me]
+        currWorkers = getAntList(currentState, me, (WORKER,))
+        #look at this current state and see if all workers are carrying?
+        for worker in currWorkers:
+            carryCount = 0
+            if worker.carrying:
+                carryCount += 1
+
+        if (carryCount == len(currWorkers)):
+            #return the state
+            return currentState # save this to an instance variable?
+
+        #food count, aka no matter where the ants are located the # of food is the main factor in states
+        foodCount = myInv.foodCount
+
+        #we sould only change this if the food count changes
+        for x in range(foodCount):
+            self.foodStates[x] = currentState
+
+        #return a state for each #?
+        for x in range(foodCount):
+            if(x == foodCount)
+
+        #new state if the queen is either on or off of the anthill.
+        myQueen = getAntList(currentState, me, (QUEEN,))[0]
+        stepsToAnthill = stepsToReach(currentState, myQueen.coords, myInv.getAnthill().coords)
+        stepsToTunnel = stepsToReach(currentState, myQueen.coords, myInv.getTunnels()[0].coords)
+
+        if (stepsToTunnel >= 2 or stepsToAnthill >=2):
+            #this is a good state that might show up a lot. save it
+            return currentState
+
+        return currentState
